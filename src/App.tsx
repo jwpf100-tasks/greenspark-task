759e52ceb351019f0a3a0cd9ff640858b826a069
jwpf100 <jwpf100@gmail.com>
2021-10-27 15:29:43 +0100

    Added content container.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
-import logo from "./logo.svg";
 
 import useFetch, { IStatus } from "./hooks/fetchData";
 import "./App.css";
 import "./scss/styles.scss";
+import Container from "./components/container/Container";
+import Header from "./components/container/Header";
 
 const App = () => {
   const { response, error, status } = useFetch({
@@ -15,15 +16,16 @@ const App = () => {
   console.log(status, "isLoading");
 
   return (
-    <div className="App">
+    <div className="App app">
       {status !== IStatus.Succeeded || response === null ? (
         <>Loading</>
       ) : (
-        <>
+        <Container>
+          <Header text="Per product widgets" />
           <div className="">{JSON.stringify(response[0])}</div>
           <div className="">{JSON.stringify(response[1])}</div>
           <div className="">{JSON.stringify(response[2])}</div>
-        </>
+        </Container>
       )}
     </div>
   );
